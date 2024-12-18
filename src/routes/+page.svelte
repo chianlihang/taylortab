e2cb0dd7e7ef4628ba8c255c5845d4097b6221bb
Chianli Hang <chianlihang@gmail.com>
2024-12-18 00:06:36 -0600

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    $:innerWidth = 1920;
+    $:innerHeight = 1080;
+</script>
+
+<svelte:window bind:innerWidth bind:innerHeight/>
+
+<!-- Hello, Taylor Tabb. -->
+<title>Taylor Tab</title>
+
+ {#if innerWidth >= innerHeight}
+    <img 
+        src="/Taylor_Tabb.png"
+        alt="Taylor Tabb"
+        style="width: 100vh;"
+    >
+{:else}
+    <img 
+        src="/Taylor_Tabb.png"
+        alt="Taylor Tabb"
+        style="width: 100vw;"
+    >
+ {/if}
+
+<style>
+    img{
+        position: absolute;
+		left: 50%;
+        bottom: 0%;
+		transform: translateX(-50%);
+    }
+</style>
